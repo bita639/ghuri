084797d57a28f019bcf5492c5e358d592ecaac86
bita639 <01625326156dj@gmail.com>
2020-12-19 03:04:52 +0600

    requirement file updated

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ from . import searchviews
 urlpatterns = [
     
     path('', searchviews.show_all_persons_page, name='search_home'), 
-    path('/user', searchviews.profile_search_filter, name='search_profile'),
+    path('user', searchviews.profile_search_filter, name='search_profile'),
 ]
